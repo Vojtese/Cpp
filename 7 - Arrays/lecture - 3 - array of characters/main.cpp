afedd149bdd84b99344f48bbacfabfe2d3d92b99
Slabý <vojtech.slaby@thermofisher.com>
2022-12-23 09:55:42 +0100

    end of arrays chapter 4

--- 7 - Arrays/lecture - 3 - array of characters/main.cpp
+++ 7 - Arrays/lecture - 3 - array of characters/main.cpp
@@ -1,18 +1,69 @@
 #include <iostream>
 /**
-  * @brief: do while
+  * @brief: array of characters
   **/
 
 int main(){
+  /*
+  //Declare array
+  char message [5] {'H', 'e', 'l', 'l', 'o'};
 
-  const unsigned int COUNT {0};
-  unsigned int i {0};  // init
+  //print out the array through looping
+  std::cout << "message : ";
+  for ( auto c : message){
+    std::cout << c;
+  }
+  std::cout << std::endl;
+  std::cout << "size : " << std::size(message) << std::endl;
 
-  do{
-    std::cout << "[" << i << "] : I Love C++" << std::endl;
-    ++i; // increment
-  }while(i < COUNT);  // test
+  //can also modify elements of the char array
 
-  
+  std::cout << std::endl;
+  std::cout << "Modify array data : " << std::endl;
+
+  message[1] = 'a';
+
+  //print out the array
+  std::cout << "message : ";
+  for ( auto c : message) {
+    std::cout << c;
+  }
+  std::cout << std::endl;
+  */
+  //cool things
+  /*
+  char message [5] {'H', 'e', 'l', 'l', 'o'}; // this has no null character appended to this!!!
+  std::cout << "message : " << message << std::endl;  // direct print out
+  */
+  //propper C string definition - \0 - null termination string - proper null termination - for c string
+  char message [] {'H', 'e', 'l', 'l', 'o', '\0'};
+  std::cout << "message : " << message << std::endl;  // direct print out
+  std::cout << "size : " << std::size(message) << std::endl;   
+  // auto fill of null termination -- not initialized positions are filled with '\0' strings, compiler append it to it
+  char message2[6] {'H', 'e', 'l', 'l', 'o'};
+  std::cout << "message2 : " << message2 << std::endl;  // direct print out
+  std::cout << "size : " << std::size(message2) << std::endl;   
+
+  //deceiving stuff
+  char message3 [] {'H', 'e', 'l', 'l', 'o'}; // this is not a c string, as there is not null character
+  std::cout << "message3 : " << message3 << std::endl;  // direct print out
+  std::cout << "size : " << std::size(message3) << std::endl;  // some garbage after hello 
+
+  // Literal C strings - with double "" -- recommended way in C++
+  std::cout << std::endl;
+  char message4 [] {"Hello"}; // An implicit '\0' character is appended to the end of the string, making it c string
+  std::cout << "message4 : " << message4 << std::endl;
+  std::cout << "size : " << std::size(message4) << std::endl;
+
+  //Can even have spaces between characters
+  std::cout << std::endl;
+  char message5[] {"Hello World!"};
+  std::cout << "message5 : " << message5 << std::endl;
+  std::cout << "size : " << std::size(message5) << std::endl;
+
+  // arrays of chars are special
+  std::cout << std::endl;
+  int numbers[] {1,2,3,4,5,6,7,8,9,0};
+  std::cout << "numbers : " << numbers << std::endl;
   return 0;
 }
--- 7 - Arrays/lecture - 4 - bounds of an array/main.cpp
+++ 7 - Arrays/lecture - 4 - bounds of an array/main.cpp
@@ -1,17 +1,24 @@
 #include <iostream>
 /**
-  * @brief: do while
+  * @brief: bounds of arrays
   **/
 
 int main(){
 
-  const unsigned int COUNT {0};
-  unsigned int i {0};  // init
+  // --*******--, where *stands for places in memory 
+  // of an array and - represent before and after an arrray in memory
+  // - symbols outside bounds of an array
+  int numbers[] {1,2,3,4,5,6,7,8,9,0};
 
-  do{
-    std::cout << "[" << i << "] : I Love C++" << std::endl;
-    ++i; // increment
-  }while(i < COUNT);  // test
+  //read beyond bounds : will read garbage or crash your program
+  std:: cout << "numbers[12]" << numbers[12] << std::endl;
+
+  //Write beyond bounds. The compiler allows it. But you don;t own
+  // the memory at index 12, so other programs may modify it and your
+  // program may read bogus data at a later time. Or you can even
+  // corrupt data used by other aprts o your program
+  numbers[12999999] = 1000;
+  std::cout << "numbers[12] : " << numbers[12] << std::endl; 
 
   
   return 0;
